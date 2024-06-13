1446d23541bacd84161229259552d028e06b93ce
gunesserkan <gunesserkan883@gmail.com>
2024-06-14 01:00:07 +0300

    Added shipping service

---
+++ shipping/src/main/java/com/gunes/shipping/ShippingApplication.java
@@ -0,0 +1,62 @@
+package com.gunes.shipping;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+public class ShippingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingApplication.class, args);
+    }
+
+}
+
+@Service
+@RequiredArgsConstructor
+class OrderService {
+    private final KafkaTemplate kafkaTemplate;
+    @Autowired
+    ShippingRepository shippingRepository;
+    @KafkaListener(topics = "prod.orders.placed", groupId = "shipping-group")
+    public void handleOrderPlacedEvent(OrderPlacedEvent event) {
+        Shipping shipping = new Shipping();
+        shipping.setOrderId(event.getOrderId());
+        this.shippingRepository.save(shipping);
+        this.kafkaTemplate.send("prod.orders.shipped",String.valueOf(shipping.getOrderId()),String.valueOf(shipping.getOrderId()));
+    }
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class OrderPlacedEvent {
+    private int orderId;
+    private String product;
+    private double price;
+}
+
+@Entity
+@Data
+class  Shipping{
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int orderId;
+}
+
+interface ShippingRepository extends CrudRepository<Shipping,Long>{
+
+}
